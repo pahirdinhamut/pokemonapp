8b3f52ae727d4a9243ea98806607e07634a4b8dd
pahirdinhamut <69786935+pahirdinhamut@users.noreply.github.com>
2022-09-05 20:59:24 +0200

    Api ready

---
+++ src/api/Weaknesses.js
@@ -0,0 +1,89 @@
+const Weaknesses = [
+  {
+    id: "1",
+    type: "bug",
+    weakness: ["fire", "rock", "flying"],
+  },
+  {
+    id: "2",
+    type: "dark",
+    weakness: ["fairy", "fighting", "bug"],
+  },
+  {
+    id: "3",
+    type: "dragon",
+    weakness: ["dragon", "ice", "fairy"],
+  },
+  {
+    id: "4",
+    type: "electric",
+    weakness: ["ground"],
+  },
+  {
+    id: "5",
+    type: "fairy",
+    weakness: ["stell", "poison"],
+  },
+  {
+    id: "6",
+    type: "fighting",
+    weakness: ["fairy", "psychic", "flying"],
+  },
+  {
+    id: "7",
+    type: "fire",
+    weakness: ["ground", "rock", "water"],
+  },
+  {
+    id: "8",
+    type: "flying",
+    weakness: ["electric", "rock", "ice"],
+  },
+  {
+    id: "9",
+    type: "ghost",
+    weakness: ["dark", "ghost"],
+  },
+  {
+    id: "10",
+    type: "grass",
+    weakness: ["flying", "bug", "ice", "poison", "fire"],
+  },
+  {
+    id: "11",
+    type: "ground",
+    weakness: ["grass", "ice", "water"],
+  },
+  {
+    id: "12",
+    type: "normal",
+    weakness: ["fighting"],
+  },
+  {
+    id: "13",
+    type: "poison",
+    weakness: ["ground", "psychic"],
+  },
+  {
+    id: "14",
+    type: "psychic",
+    weakness: ["bug", "dark", "ghost"],
+  },
+  {
+    id: "15",
+    type: "rock",
+    weakness: ["grass", "water", "fighting", "ground", "steel"],
+  },
+  {
+    id: "16",
+    type: "steel",
+    weakness: ["ground", "fire", "fighting"],
+  },
+  {
+    id: "17",
+    type: "water",
+    weakness: ["electric", "grass"],
+  },
+];
+
+export default Weaknesses;
