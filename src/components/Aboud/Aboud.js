3d56a6aaf939568966a2c6b46babf29478a78246
pahirdinhamut <69786935+pahirdinhamut@users.noreply.github.com>
2022-09-05 10:47:31 +0200

    aboud conpomenet

--- src/components/Aboud/Aboud.js
+++ src/components/Aboud/Aboud.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { colors, textColor } from "../../../assets/COLORS/Color";
+import Space from "../Space";
 
 const Aboud = ({ pokemon }) => {
   return (
@@ -15,6 +16,29 @@ const Aboud = ({ pokemon }) => {
       >
         Pokédex Data
       </Text>
+      {/* Data */}
+      <View style={{ marginVertical: 5 }}>
+        <View style={{ flexDirection: "row", marginVertical: 5 }}>
+          <Text style={styles.discription}>Species</Text>
+          <Space size={30} />
+          <Text>Seed Pokémon</Text>
+        </View>
+        <View style={{ flexDirection: "row", marginVertical: 5 }}>
+          <Text style={styles.discription}>Height</Text>
+          <Space size={35} />
+          <Text>{pokemon.height / 10}m</Text>
+        </View>
+        <View style={{ flexDirection: "row", marginVertical: 5 }}>
+          <Text style={styles.discription}>weight</Text>
+          <Space size={35} />
+          <Text>{pokemon.weight / 10}Kg</Text>
+        </View>
+        <View style={{ flexDirection: "row", marginVertical: 5 }}>
+          <Text style={styles.discription}>Abilities</Text>
+          <Space size={30} />
+          <Text>{pokemon.abilities[0].ability.name}</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -32,6 +56,10 @@ const styles = StyleSheet.create({
   Title: {
     fontS: 16,
     fontWeight: "700",
+    marginTop: 10,
+  },
+  discription: {
+    fontSize: 12,
   },
 });
 export default Aboud;
