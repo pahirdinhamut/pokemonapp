9a5479242f5e30afbfdbea4466ae1568801ec22c
pahirdinhamut <69786935+pahirdinhamut@users.noreply.github.com>
2022-09-06 19:48:00 +0200

    generation json file

---
+++ src/api/Generation.js
@@ -0,0 +1,36 @@
+const Generation = [
+  {
+    id: "1",
+    generation: "generation I",
+  },
+  {
+    id: "2",
+    generation: "generation II",
+  },
+  {
+    id: "3",
+    generation: "generation III",
+  },
+  {
+    id: "4",
+    generation: "generation IV",
+  },
+  {
+    id: "5",
+    generation: "generation V",
+  },
+  {
+    id: "6",
+    generation: "generation VI",
+  },
+  {
+    id: "7",
+    generation: "generation VII",
+  },
+  {
+    id: "8",
+    generation: "generation VIII",
+  },
+];
+
+export default Generation;
